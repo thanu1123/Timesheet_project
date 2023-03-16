0d36835dea397460023a7b0c00d74db7ab9d3134
thanu1123 <126650082+thanu1123@users.noreply.github.com>
2023-03-16 17:29:02 +0530

    authorization for Timesheet

--- TimesheetPoject/Controllers/UploadController.cs
+++ TimesheetPoject/Controllers/UploadController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TimesheetPoject.Interface;
 using TimesheetPoject.Model;
 
 namespace TimesheetPoject.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class UploadController : ControllerBase
